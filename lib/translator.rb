7351d03aa3d7f6269be398439549814ba425241d
Kai Peng Li <oopanpan@gmail.com>
2020-12-24 19:07:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library("./lib/emoticons.yml")
   # code goes here
+  code = YAML.load_file("lib/emoticons.yml")
+  result = {}
+  code.each do |key, emoji|
+    if !result[key]
+      result[key] = {:english => emoji[0], :japanese => emoji[1] }
+    end
+  end
+  binding.pry
+  result
 end
 
 def get_japanese_emoticon
@@ -11,3 +22,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+load_library
